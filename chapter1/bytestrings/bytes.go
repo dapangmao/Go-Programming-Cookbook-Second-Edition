2e3779270278a18f71f4196af93de305fca744fa
Dapangmao <hchao8@gmail.com>
2020-09-02 13:36:13 -0400

    Update bytes.go

--- chapter1/bytestrings/bytes.go
+++ chapter1/bytestrings/bytes.go
@@ -10,27 +10,7 @@ import (
 // Buffer function
 func WorkWithBuffer() error {
 	rawString := "it's easy to encode unicode into a byte array ❤️"
-
-	b := Buffer(rawString)
-
-	// we can quickly convert a buffer back into byes with
-	// b.Bytes() or a string with b.String()
-	fmt.Println(b.String())
-
-	// because this is an io Reader we can make use of generic
-	// io reader functions such as
-	s, err := toString(b)
-	if err != nil {
-		return err
-	}
-	fmt.Println(s)
-
-	// we can also take our bytes and create a bytes reader
-	// these readers implement io.Reader, io.ReaderAt,
-	// io.WriterTo, io.Seeker, io.ByteScanner, and io.RuneScanner
-	// interfaces
-	reader := bytes.NewReader([]byte(rawString))
-
+	reader := strings.NewReader(rawString)
 	// we can also plug it into a scanner that allows buffered
 	// reading and tokenzation
 	scanner := bufio.NewScanner(reader)
@@ -41,5 +21,5 @@ func WorkWithBuffer() error {
 		fmt.Print(scanner.Text())
 	}
 
-	return nil
+	return scanner.Err()
 }
