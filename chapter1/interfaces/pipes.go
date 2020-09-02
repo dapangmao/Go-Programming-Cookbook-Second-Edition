cae40c9af719cf4c8216c0c62f1abf4d5d83fa12
Dapangmao <hchao8@gmail.com>
2020-09-02 13:48:09 -0400

    Update pipes.go

--- chapter1/interfaces/pipes.go
+++ chapter1/interfaces/pipes.go
@@ -3,27 +3,23 @@ package interfaces
 import (
 	"io"
 	"os"
+	"time"
 )
 
-// PipeExample helps give some more examples of using io interfaces
-func PipeExample() error {
-	// the pipe reader and pipe writer implement
-	// io.Reader and io.Writer
-	r, w := io.Pipe()
+func reader(r *io.PipeReader) {
+	io.Copy(os.Stdout, r)
+}
 
-	// this needs to be run in a separate go routine
-	// as it will block waiting for the reader
-	// close at the end for cleanup
-	go func() {
-		// for now we'll write something basic,
-		// this could also be used to encode json
-		// base64 encode, etc.
+func write(w *io.PipeWriter) {
+	for {
+		<-time.Tick(time.Second * 1)
 		w.Write([]byte("test\n"))
-		w.Close()
-	}()
-
-	if _, err := io.Copy(os.Stdout, r); err != nil {
-		return err
 	}
-	return nil
+}
+
+func main() {
+	r, w := io.Pipe()
+	go reader(r)
+	write(w)
+
 }
